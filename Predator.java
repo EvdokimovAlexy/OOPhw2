ec9149b0590f8e9ce738095953515834f1cb2a0a
Prometheus <poligraphich@gmail.com>
2023-02-19 22:12:05 +0300

    hishniki

---
+++ Predator.java
@@ -0,0 +1,9 @@
+package org.example;
+public abstract class Predator extends Animal{
+    public Predator(String nickname) {
+        super(nickname);
+    }
+    public String feed() {
+        return "meat";
+    }
+}
