426b8bb4eb25350292317586c832c619aa8958ab
Prometheus <poligraphich@gmail.com>
2023-02-19 22:09:57 +0300

    add travoyzdny

---
+++ Herbivores.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public abstract class Herbivores extends Animal{
+    public Herbivores(String nickname) {
+        super(nickname);
+    }
+
+    public String feed(){
+        return "herb";
+    }
+}
